80ddef6e76d45279a4eedfe04071b89bb66103ea
hemish-p <133946031+hemish-p@users.noreply.github.com>
2026-02-06 20:28:35 +0530

    1.py

---
+++ Day-3/1.py
@@ -0,0 +1,10 @@
+'''
+Write a program where the user enters a number, and the program calculates its square and cube. For example:
+
+Input: Enter a number: 3
+Output: Square: 9, Cube: 27
+'''
+
+num= int(input("Enter the number "))
+
+print(f"Square : {num**2}, Cube: {num**3}")
