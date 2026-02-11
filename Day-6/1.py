12c9459938cae474078667043b576e20e76c4b3f
hemish-p <133946031+hemish-p@users.noreply.github.com>
2026-02-11 18:52:24 +0530

    1.py

---
+++ Day-6/1.py
@@ -0,0 +1,14 @@
+'''
+Pattern 1: Sqaure
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+
+
+for i in range(1,6):      #u can use input() func 
+    for j in range(1,6):
+        print("* ",end="")
+    print()
